00c4b44e34154aa44f34aa323e538a8431354920
Gepeng18 <robin_ge@foxmail.com>
2020-03-31 17:54:38 +0800

    add backgound im

--- community/js/letter.js
+++ community/js/letter.js
@@ -9,7 +9,7 @@ function send_letter() {
 	var toName = $("#recipient-name").val();
 	var content = $("#message-text").val();
 	$.post(
-	    CONTEXT_PATH + "/letter/send",
+	    CONTEXT_PATH + "/letter",
 	    {"toName":toName,"content":content},
 	    function(data) {
 	        data = $.parseJSON(data);
